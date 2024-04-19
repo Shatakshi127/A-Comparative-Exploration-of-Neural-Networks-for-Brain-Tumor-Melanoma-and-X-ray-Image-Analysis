e7e50ff8b8f3bdc65383f819de5fb7d3c96048ec
Shatakshi127 <107629085+Shatakshi127@users.noreply.github.com>
2024-04-19 19:50:47 +0530

    pneumonia models

--- PNEUMONIA_DATASET/efficientnetb.py
+++ PNEUMONIA_DATASET/efficientnetb.py
@@ -180,3 +180,72 @@ plt.show()
 
 from sklearn.metrics import classification_report
 print(classification_report(test_generator.classes, labels))
+
+#GradCam
+def grad_cam_heatmap(image, last_conv_layer_name='last_conv'):
+    
+    if model.layers[0].__class__.__name__ == 'Functional':
+        last_conv_layer_idx = 0
+        last_conv_layer_model = model.layers[0]
+    else:
+        last_conv_layer = model.get_layer(last_conv_layer_name)
+        last_conv_layer_idx = model.layers.index(last_conv_layer)
+        last_conv_layer_model = tf.keras.Model(model.inputs, last_conv_layer.output)
+
+
+    classifier_input = tf.keras.Input(shape=last_conv_layer_model.output.shape[1:])
+    x = classifier_input
+    classifier_layers = model.layers[last_conv_layer_idx+1:]
+    for layer in classifier_layers:
+        x = layer(x)
+    classifier_model = tf.keras.Model(classifier_input, x)
+
+
+    with tf.GradientTape() as tape:
+        # Compute activations of the last conv layer and make the tape watch it
+        last_conv_layer_output = last_conv_layer_model(image)
+        tape.watch(last_conv_layer_output)
+        # Compute class predictions
+        preds = classifier_model(last_conv_layer_output)
+        top_pred_index = tf.argmax(preds[0])
+        top_class_channel = preds[:, top_pred_index]
+        print(CLASSES[top_pred_index])
+
+    # This is the gradient of the top predicted class with regard to
+    # the output feature map of the last conv layer
+    grads = tape.gradient(top_class_channel, last_conv_layer_output)
+
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    last_conv_layer_output = last_conv_layer_output.numpy()[0]
+    pooled_grads = pooled_grads.numpy()
+    for i in range(pooled_grads.shape[-1]):
+        last_conv_layer_output[:, :, i] *= pooled_grads[i]
+
+    # The channel-wise mean of the resulting feature map
+    # is our heatmap of class activation
+    heatmap = np.mean(last_conv_layer_output, axis=-1)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
+
+    return heatmap
+
+import matplotlib.cm as cm
+
+def grad_cam(img, last_conv_layer_name='last_conv'):
+    colors = cm.jet(np.arange(256))[:, :3]
+    gc_mask = grad_cam_heatmap(np.expand_dims(img, 0), last_conv_layer_name)
+    gc_mask_uint8 = (gc_mask*255.0).astype('uint8')
+    heatmap = colors[gc_mask_uint8]
+    heatmap = cv2.resize(heatmap, (img_dim, img_dim))
+    heatmap = (heatmap*255).astype('uint8')
+    img_uint8 = img.astype('uint8')
+    img_overlay = cv2.addWeighted(src1=img_uint8, alpha=0.6, src2=heatmap, beta=0.4, gamma=0.0)
+    plt.imshow(img_overlay)
+    plt.show()
